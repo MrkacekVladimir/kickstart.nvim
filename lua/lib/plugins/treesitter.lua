88b1c73b78f619aef553c68a3e2c3ac5a690fea1
Vladimír Mrkáček <vlad.mrkacek@gmail.com>
2024-12-27 20:23:46 +0100

    Changes to C# dev

---
+++ lua/lib/plugins/treesitter.lua
@@ -0,0 +1,51 @@
+return {
+  'nvim-treesitter/nvim-treesitter',
+  build = ':TSUpdate',
+  main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+  event = 'VeryLazy',
+  lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
+  opts = {
+    auto_install = true,
+    ensure_installed = {
+      'bash',
+      'c',
+      'c_sharp',
+      'diff',
+      'html',
+      'javascript',
+      'jsdoc',
+      'json',
+      'jsonc',
+      'lua',
+      'luadoc',
+      'luap',
+      'markdown',
+      'markdown_inline',
+      'printf',
+      'python',
+      'query',
+      'regex',
+      'toml',
+      'tsx',
+      'typescript',
+      'vim',
+      'vimdoc',
+      'xml',
+      'yaml',
+    },
+    indent = { enable = true },
+    highlight = {
+      enable = true,
+      disable = { 'latex' },
+    },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = '<C-space>',
+        node_incremental = '<C-space>',
+        scope_incremental = false,
+        node_decremental = '<bs>',
+      },
+    },
+  },
+}
---
+++ lua/lib/plugins/utils.lua
@@ -0,0 +1,15 @@
+return {
+  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  {
+    'echasnovski/mini.nvim',
+    config = function()
+      local statusline = require 'mini.statusline'
+      statusline.setup { use_icons = vim.g.have_nerd_font }
+
+      ---@diagnostic disable-next-line: duplicate-set-field
+      statusline.section_location = function()
+        return '%2l:%-2v'
+      end
+    end,
+  },
+}
